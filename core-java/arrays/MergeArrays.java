60e943384294c469bdc63a3a292bed41f5e49b40
Mohsen Khademian <90333755+Mohsenkhademian@users.noreply.github.com>
2023-01-03 00:40:38 +0330

    Update MergeArrays.java
    you can use stream

--- core-java/arrays/MergeArrays.java
+++ core-java/arrays/MergeArrays.java
@@ -28,3 +28,30 @@ public class MergeArrays {
 	}
 
 }
+
+/////////////////////////////////////////////////////////////
+
+/*
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class MergeArrays {
+    public static <T> List<T> concatenate(T[] a, T[] b, Comparator<? super T> comparator) {
+        return Stream.concat(Arrays.stream(a), Arrays.stream(b))
+                     .sorted(comparator)
+                     .collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        Integer[] a = new Integer[]{1, 2, 3};
+        Integer[] b = new Integer[]{4, 5, 6};
+
+        List<Integer> c = concatenate(a, b, (x, y) -> y - x);
+
+        System.out.println("Merged and sorted array: " + c);
+    }
+}
+
+*/
