bffe9fcd3b759ea84e43b5e19a0ca72d6fd88173
Mohsen Khademian <90333755+Mohsenkhademian@users.noreply.github.com>
2023-01-03 16:47:54 +0330

    Update SortSubarray.java
    you can use ArrayList

--- core-java/basics/SortSubarray.java
+++ core-java/basics/SortSubarray.java
@@ -12,3 +12,23 @@ public class SortSubarray {
 		System.out.printf("Sorted Array: %s ", Arrays.toString(arr));
 	}
 }
+
+
+// if use List :
+
+/*
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SortSubarray {
+    public static void main(String[] args) {
+        List<Integer> arr = new ArrayList<>(Arrays.asList(13, 7, 6, 45, 21, 9, 2, 100));
+        Collections.sort(arr.subList(0, 5));
+        System.out.println("Sorted Array: " + arr);
+    }
+}
+
+
+*/
